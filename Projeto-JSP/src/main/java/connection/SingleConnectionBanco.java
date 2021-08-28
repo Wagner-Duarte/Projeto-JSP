d2e0087cae912c1a5cccb2d0d61de5ea251739ed
Wagner <duartti@gmail.com>
2021-08-28 08:12:46 -0300

    Adicionado projeto JSP + Servlet

---
+++ Projeto-JSP/src/main/java/connection/SingleConnectionBanco.java
@@ -0,0 +1,45 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnectionBanco {
+	
+	private static String banco = "jdbc:postgresql://localhost:5432/projeto-jsp?autoReconnect=true";
+	
+	private static String user = "postgres";
+	private static String senha = "21020519";
+	private static Connection connection = null;
+	
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	static {
+		connectDB();
+	}
+	
+	
+	public SingleConnectionBanco() {
+		connectDB();
+	}
+	
+	private static void connectDB() {
+		
+		try {
+			
+			if(connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(banco, user, senha);
+				connection.setAutoCommit(false);
+				// System.out.println("deu conexao ");
+			}
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace(); // mostar qualquer erro na conexão.
+		}
+	}
+
+}
